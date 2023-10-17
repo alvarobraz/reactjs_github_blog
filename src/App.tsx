e91997cfae6e29a9f15b9140d527b30706b159b7
Álvaro Braz <alvarobraz83@gmail.com>
2023-10-17 18:55:30 -0300

    added app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./src/styles/themes/default";
+import { GlobalStyle } from "./src/styles/global";
+
+
 export function App() {
   
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Heloo world!</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
+
---
+++ src/src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
+
+  body {
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 0.875rem Nunito, sans-serif;
+  }
+`;
